d327dad9322ce4a4cb9f6898b0c1364cd2b28c6a
Carter Parrett <cparrett94@hotmail.com>
2020-07-20 20:39:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,14 +18,11 @@ class Song
   
   def artist_name
     self.artist.name
-    if artist = nil
+    if artist == nil
       return nil
     else
       self.artist.name
     end
-  
-
-    
   end
     
    
